9db8bc377197536a536c1ed4009d2f2bfbb10683
Thibaut Van Callemont <vancallemont.thibaut@gmail.com>
2020-09-14 15:59:04 +0200

    Ajout shader Ciconia Studio + Sprites divers

---
+++ Scripts/CustomButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+	
+	private Image activeImg;
+	private ScreenManager screenManager;
+	public int idUnderMenu = 0;
+
+	// Use this for initialization
+	void Start () {
+		activeImg = GetComponent<Image>();
+		screenManager = GameObject.Find("Screen").GetComponent<ScreenManager>();
+	}
+	
+	public void OnPointerEnter(PointerEventData eventData){
+		activeImg.enabled = true;
+		//Debug.Log("Cercle actif");
+	}
+   
+	public void OnPointerExit(PointerEventData eventData){
+		activeImg.enabled = false;
+		//Debug.Log("Cercle inactif");
+	}
+   
+	public void OnPointerClick(PointerEventData eventData){
+	   //on agrandit la fenêtre de gauche et on affiche le contenu détaillé
+	   screenManager.SetIdUnderMenu(idUnderMenu);
+	   screenManager.SetIdMenuAnimation(3);
+	   //Debug.Log("Sous-menu ouvert");
+	}
+}
---
+++ Scripts/Fight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fight : MonoBehaviour {
+	
+	private bool startBattle = false;
+	private bool battleFirstFrame = true;
+	public Camera fightCam;
+	public Camera mainCam;
+	
+	// Use this for initialization
+	void Start(){
+		mainCam.enabled = true;
+		fightCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if(startBattle == true){
+			if(battleFirstFrame == true){
+				mainCam.enabled = false;
+				fightCam.enabled = true;
+				battleFirstFrame = false;
+			}
+			
+			if (Input.GetKeyDown(KeyCode.C)) {
+				mainCam.enabled = !mainCam.enabled;
+				fightCam.enabled = !fightCam.enabled;
+			}
+		}
+	}
+	
+	public void SetStartBattle(bool newValue){
+		startBattle = newValue;
+		return;
+	}
+	
+	public bool GetStartBattle(){
+		return startBattle;
+	}
+}
